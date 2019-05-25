10c9be0bbabf8a38dbf97d26cc002e1ab99660f2
马三小伙儿 <1790617178@qq.com>
2019-05-25 20:31:42 +0800

    完善UGUI_Model类

--- Assets/Scripts/UIBase/UGUIModel.cs
+++ Assets/Scripts/UIBase/UGUIModel.cs
@@ -11,6 +11,29 @@ using UnityEngine.EventSystems;
 [RequireComponent(typeof(RectTransform),typeof(EmptyRaycast))]
 public class UGUIModel : UIBehaviour, IDragHandler
 {
+    /// <summary>
+    /// 模型的LayerName
+    /// </summary>
+    private const string UIModelLayerName = "UI_Model";
+
+    [SerializeField]
+    private Vector3 scale = Vector3.one;
+    [SerializeField]
+    private float positionX = 0.0f;
+    [SerializeField]
+    private float positionY = 0.0f;
+    [SerializeField]
+    private float modelOffsetX = 0.0f;
+    [SerializeField]
+    private float modelOffsetY = 0.0f;
+
+    [SerializeField]
+    private float cameraDistance = 3.0f;
+    [SerializeField]
+    private float cameraHeightOffset = 0.0f;
+    [SerializeField]
+    private bool enableRotate = true;
+
     public void OnDrag(PointerEventData eventData)
     {
 
