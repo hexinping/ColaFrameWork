c853ffa459da417a5c57e6a0bd00b5ba240b75ae
马三小伙儿 <1790617178@qq.com>
2019-05-28 21:33:25 +0800

    UGUIModel添加属性注释

--- Assets/Scripts/UIBase/UGUIModel.cs
+++ Assets/Scripts/UIBase/UGUIModel.cs
@@ -17,21 +17,29 @@ public class UGUIModel : UIBehaviour, IDragHandler
     private const string UIModelLayerName = "UI_Model";
 
     [SerializeField]
+    [Tooltip("模型的缩放")]
     private Vector3 scale = Vector3.one;
     [SerializeField]
+    [Tooltip("模型的X坐标")]
     private float positionX = 0.0f;
     [SerializeField]
+    [Tooltip("模型的Y坐标")]
     private float positionY = 0.0f;
     [SerializeField]
+    [Tooltip("模型的X轴偏移量")]
     private float modelOffsetX = 0.0f;
     [SerializeField]
+    [Tooltip("模型的Y轴偏移量")]
     private float modelOffsetY = 0.0f;
 
     [SerializeField]
+    [Tooltip("相机距离模型的距离")]
     private float cameraDistance = 3.0f;
     [SerializeField]
+    [Tooltip("相机相对模型高度")]
     private float cameraHeightOffset = 0.0f;
     [SerializeField]
+    [Tooltip("模型是否可以旋转")]
     private bool enableRotate = true;
 
     public void OnDrag(PointerEventData eventData)
