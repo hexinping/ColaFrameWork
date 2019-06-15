1fa4cf24b1f58a3ce77b28e31a0322566b64fed2
马三小伙儿 <1790617178@qq.com>
2019-06-15 15:42:41 +0800

    添加UGUIModel组件的编辑器拓展类

---
+++ Assets/Editor/InspectorExt/UGUIModelInspector.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using ColaFrame;
+
+
+/// <summary>
+/// UGUIModel组件的Inspector编辑器拓展
+/// </summary>
+[CustomEditor(typeof(UGUIModel), true)]
+public class UGUIModelInspector : InspectorBase
+{
+    protected override void OnEnable()
+    {
+        ShowCustomProperties = true;
+    }
+
+    protected override void DrawCustomGUI()
+    {
+        if (GUILayout.Button("导入设置"))
+        {
+            this.OnEnable();
+            Debug.Log("----->导入设置");
+        }
+
+        if (GUILayout.Button("导出设置"))
+        {
+            Debug.Log("----->导出设置");
+        }
+
+        if (GUILayout.Button("恢复默认设置"))
+        {
+            Debug.Log("----->恢复默认设置");
+        }
+    }
+}
