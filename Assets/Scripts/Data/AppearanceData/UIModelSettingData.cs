9206704c5962c0bec7e19b94e5e6dd680ad4da92
马三小伙儿 <1790617178@qq.com>
2019-06-17 16:41:30 +0800

    新增AppearanceData/UIModelSettingData并进行新建配置文件测试

---
+++ Assets/Scripts/Data/AppearanceData/UIModelSettingData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New UIModelSetting", menuName = "ColaFrame/UIModelSettingData", order = 1)]
+public class UIModelSettingData : ScriptableObject
+{
+    [Tooltip("配置文件对应的key")]
+    public string key;
+    [Tooltip("模型文件资源的相对路径")]
+    public string modelResPath;
+
+    public float cameraPitch;
+    public float cameraYaw;
+    public float cameraDistance;
+    public float cameraHeightOffset;
+    public float modelCameraDepth;
+    public float positionX;
+    public float positionZ;
+}
