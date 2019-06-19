8f5d359b8bf3781933c334a2afb169be94bc8ba3
马三小伙儿 <1790617178@qq.com>
2019-06-19 20:35:29 +0800

    UIModelSettingData添加默认值

--- Assets/Scripts/Data/AppearanceData/UIModelSettingData.cs
+++ Assets/Scripts/Data/AppearanceData/UIModelSettingData.cs
@@ -8,11 +8,11 @@ public class UIModelSettingData : ScriptableObject
     [Tooltip("模型文件资源的相对路径")]
     public string modelResPath;
 
-    public float cameraPitch;
-    public float cameraYaw;
-    public float cameraDistance;
-    public float cameraHeightOffset;
-    public float modelCameraDepth;
-    public float positionX;
-    public float positionZ;
+    public float cameraPitch = 0;
+    public float cameraYaw = 90;
+    public float cameraDistance = 7;
+    public float cameraHeightOffset = 0.47f;
+    public float modelCameraDepth = 6;
+    public float positionX = 0;
+    public float positionZ = 0;
 }
