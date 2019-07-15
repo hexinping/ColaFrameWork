1401c2059b5aae96cc2fb0d8a0731d4dbba06034
马三小伙儿 <1790617178@qq.com>
2019-07-15 17:09:09 +0800

    添加MainCamerCtrl主相机控制器

--- Assets/Scripts/Game/Camera/MainCameraCtrl.cs
+++ Assets/Scripts/Game/Camera/MainCameraCtrl.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-public class ThirdCamera : MonoBehaviour
+public class MainCameraCtrl : MonoBehaviour
 {
     public Transform target = null;     // 目标玩家
     [SerializeField]
