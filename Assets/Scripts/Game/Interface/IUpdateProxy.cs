9656a1aef1a7710af0b14fd5dee34d6f94a2777c
马三小伙儿 <1790617178@qq.com>
2019-07-18 21:05:41 +0800

    添加IUpdateProxy更新代理器接口

---
+++ Assets/Scripts/Game/Interface/IUpdateProxy.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 可以用于游戏内更新的Proxy
+/// </summary>
+public interface IUpdateProxy
+{
+    float TimeSinceUpdate { get; set; }
+    void Update(float deltaTime);
+}
