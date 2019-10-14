2228879f510ccb7b889d4c3ecbaa88a26b5fbbaa
XinCGer <1790617178@qq.com>
2019-10-14 20:59:14 +0800

    添加NetManager

---
+++ Assets/Lua/Core/Net/NetManager.lua
@@ -0,0 +1,38 @@
+---
+--- ColaFramwork的Lua端Network管理器
+---
+
+local sproto = require "3rd/sproto/sproto"
+local core = require "sproto.core"
+local print_r = require "3rd/sproto/print_r"
+
+local NetManager = {}
+
+--- NetManager的初始化
+function NetManager.Initialize()
+end
+
+--- NetManager尝试连接服务器
+function NetManager.Connect(ip,port,callback)
+
+end
+
+--- 监听网络协议
+function NetManager.Register(protoID,callback)
+
+end
+
+--- 取消监听网络协议
+function NetManager.UnRegister(protoID)
+
+end
+
+--- 取消监听所有的网络协议
+function NetManager.UnRegisterAll()
+
+end
+
+--- 关闭网络连接
+function NetManager.Close(callback)
+
+end
