cfeeb54848e3ebf91e25cae937c3cd1aafc82f03
XinCGer <1790617178@qq.com>
2019-10-23 20:27:27 +0800

    Revert "移除ProtocalDef类"
    This reverts commit 6650c5d4ab9ad79f2455ad0526f735561f41c5bf.

---
+++ Assets/Scripts/NetWork/Socket/ProtocalDefinition.cs
@@ -0,0 +1,13 @@
+﻿//----------------------------------------------
+//            ColaFramework
+// Copyright © 2018-2049 ColaFramework 马三小伙儿
+//----------------------------------------------
+
+/// <summary>
+/// 网络事件协议ID
+/// </summary>
+public enum eProtocalCommand
+{
+    sc_binary_login = 0x1000,
+    sc_protobuf_login = 0x2000,
+}
