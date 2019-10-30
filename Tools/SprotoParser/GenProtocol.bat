5a09a7d00ce22c8bdf5ef1c001396efd80da9743
XinCGer <1790617178@qq.com>
2019-10-30 20:56:40 +0800

    提交批处理

--- Tools/SprotoParser/GenProtocol.bat
+++ Tools/SprotoParser/GenProtocol.bat
@@ -1,2 +1,16 @@
-rem 待完善
+@chcp 65001
+set base_dir=%~dp0
+%base_dir:~0,2%
+cd %base_dir%
+
+set sproto_src_dir = .\Protocols\
+set sproto_dst_dir = .\Outputs\
+@echo 开始执行sproto协议导出...
+set file_list = hao1
+set split_char = 
+
+for %%i in (.\Protocols\*.sproto) do ( 
+	echo %i%
+)
+
 @pause
