0b51bbd48b629a8500f518e677c2d4c073cb5342
XinCGer <1790617178@qq.com>
2019-10-31 20:39:53 +0800

    添加转协议工具的使用指南 测试版

--- Tools/SprotoParser/GenProtocol.bat
+++ Tools/SprotoParser/GenProtocol.bat
@@ -6,11 +6,8 @@ cd %base_dir%
 set sproto_src_dir = .\Protocols\
 set sproto_dst_dir = .\Outputs\
 @echo 开始执行sproto协议导出...
-set file_list = hao1
-set split_char = 
 
-for %%i in (.\Protocols\*.sproto) do ( 
-	echo %i%
-)
+rem 工作都转移到Lua中实现吧
 
 @pause
+
