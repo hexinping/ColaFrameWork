0f295d6e22abf59b29dcf7222133aa938a27f940
XinCGer <1790617178@qq.com>
2019-11-01 20:38:59 +0800

    添加C#版的SprotoParser

---
+++ Tools/Src/SprotoParser/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace SprotoParser
+{
+    class Program
+    {
+        private static readonly string protosPath = @"./Protocols";
+        private static List<String> fileNameList = new List<String>();
+        private static readonly string extName = ".sproto";
+        private static string workDir = Directory.GetCurrentDirectory();
+
+        static void Main(string[] args)
+        {
+            ScanSprotosName(protosPath);
+            Console.WriteLine(workDir);
+        }
+
+        public static void ScanSprotosName(string path)
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(path);
+            FileSystemInfo[] fileInfos = dirInfo.GetFileSystemInfos();
+            foreach (FileSystemInfo fsinfo in fileInfos)
+            {
+                if (fsinfo is DirectoryInfo)
+                {
+                    ScanSprotosName(fsinfo.FullName);
+                }
+                else
+                {
+                    if (fsinfo.Extension == extName)
+                    {
+                        var fullName = fsinfo.FullName;
+                        fullName = fullName.Replace(workDir, ".");
+                        fullName = fullName.Replace(@"\", @"/");
+                        Console.WriteLine(fullName);
+                        fileNameList.Add(fsinfo.Name);
+                    }
+                }
+            }
+        }
+    }
+}
