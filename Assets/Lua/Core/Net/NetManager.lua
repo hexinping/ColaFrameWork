21d0b27713ceeae33ce0e520c471964e4d914eda
XinCGer <1790617178@qq.com>
2019-11-04 20:53:51 +0800

    加载sproto.bytes并测试可用性

--- Assets/Lua/Core/Net/NetManager.lua
+++ Assets/Lua/Core/Net/NetManager.lua
@@ -6,21 +6,29 @@
 
 local sproto = require "3rd/sproto/sproto"
 local core = require "sproto.core"
+local Protocol = require("Protocols.Protocol")
 
 local NetManager = {}
 
 local ByteBuffer = ByteBuffer
 local luabuffer = ByteBuffer.New()
+local sprotoCoder = nil
+local SPROTO_BYTES_PATH = "SprotoBytes/sproto.bytes"
 
 --- NetManager的初始化
 function NetManager.Initialize()
-    luabuffer:WriteInt(1)
-    luabuffer:WriteString("abc")
+    local sprotoBytes = Common_Utils.LoadTextWithBytes(SPROTO_BYTES_PATH)
+    sprotoCoder = sproto.new(sprotoBytes)
+
+    --测试bytes
+    local person = sprotoCoder:default "Person"
+    print(person)
+    -- TODO:配置网络加密等
 end
 
 --- NetManager尝试连接服务器
 function NetManager.Connect(ip,port,callback)
-
+    print("-------->try to connect:",ip,port)
 end
 
 --- 监听网络协议
@@ -40,6 +48,16 @@ end
 
 --- 关闭网络连接
 function NetManager.Close(callback)
+
+end
+
+--- 分帧处理网络消息
+function NetManager.Update()
+
+end
+
+--- 处理C#端传到Lua端的消息
+function NetManager.OnMessage()
 
 end
 
