a93b3d68d844e64275b48b269614bec55335bac7
XinCGer <1790617178@qq.com>
2019-11-05 20:38:12 +0800

    完善NetManager

--- Assets/Lua/Core/Net/NetManager.lua
+++ Assets/Lua/Core/Net/NetManager.lua
@@ -9,26 +9,33 @@ local core = require "sproto.core"
 local Protocol = require("Protocols.Protocol")
 
 local NetManager = {}
-
+local Socket = nil
 local ByteBuffer = ByteBuffer
 local luabuffer = ByteBuffer.New()
 local sprotoCoder = nil
 local SPROTO_BYTES_PATH = "SprotoBytes/sproto.bytes"
 
+local OnConnectedCallback = nil
+
 --- NetManager的初始化
 function NetManager.Initialize()
+    Socket = SocketManager.Instance
     local sprotoBytes = Common_Utils.LoadTextWithBytes(SPROTO_BYTES_PATH)
     sprotoCoder = sproto.new(sprotoBytes)
 
-    --测试bytes
-    local person = sprotoCoder:default "Person"
-    print(person)
+    Socket.OnConnected = NetManager.OnConnected
+    Socket.OnReConnected = NetManager.OnReConnected
+    Socket.OnClose = NetManager.OnClosed
+    Socket.OnFailed = NetManager.OnFailed
+    Socket.OnTimeOut = NetManager.OnTimeOut
+    --NetMessageCenter.OnMessage = NetManager.OnMessage
     -- TODO:配置网络加密等
 end
 
 --- NetManager尝试连接服务器
 function NetManager.Connect(ip,port,callback)
     print("-------->try to connect:",ip,port)
+    OnConnectedCallback = callback
 end
 
 --- 监听网络协议
@@ -57,8 +64,39 @@ function NetManager.Update()
 end
 
 --- 处理C#端传到Lua端的消息
-function NetManager.OnMessage()
+function NetManager.OnMessage(byteMsg)
 
+end
+
+--- 处理Socket成功连接服务器
+function NetManager.OnConnected()
+    if nil ~= OnConnectedCallback then
+        OnConnectedCallback()
+    end
+end
+
+--- 处理网络重连
+function NetManager.OnReConnected()
+
+end
+
+--- 处理网络关闭
+function NetManager.OnClosed()
+
+end
+
+--- 连接服务器失败
+function NetManager.OnFailed()
+
+end
+
+--- 连接服务器超时
+function NetManager.OnTimeOut()
+
+end
+
+function NetManager.Tick(deltaTime)
+    print("------------>Update",deltaTime)
 end
 
 return NetManager
