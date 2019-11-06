74a9b455788768ad191cb6be01257874b6187f0c
XinCGer <1790617178@qq.com>
2019-11-06 20:09:01 +0800

    完善NetMeassageCenter

--- Assets/Scripts/NetWork/Socket/MessageCenter.cs
+++ Assets/Scripts/NetWork/Socket/MessageCenter.cs
@@ -1,6 +1,6 @@
 ﻿/// <summary>
 /// 网络消息处理中心
-/// 
+/// 缓存消息，然后分帧泵到lua端进行处理
 /// </summary>
 
 using UnityEngine;
@@ -15,7 +15,7 @@ public struct sEvent_NetMessageData
     public byte[] _eventData;
 }
 
-public class NetMessageCenter : SingletonMonoBehaviour<NetMessageCenter>,IManager
+public class NetMessageCenter : SingletonMonoBehaviour<NetMessageCenter>, IManager
 {
     [LuaInterface.NoToLua]
     public Queue<sEvent_NetMessageData> _netMessageDataQueue;
@@ -26,7 +26,7 @@ public class NetMessageCenter : SingletonMonoBehaviour<NetMessageCenter>,IManage
     /// <summary>
     /// 每帧默认处理2个协议
     /// </summary>
-    private int perHandleCnt = 3; 
+    private int perHandleCnt = 3;
 
     public float TimeSinceUpdate { get; set; }
 
@@ -49,7 +49,18 @@ public class NetMessageCenter : SingletonMonoBehaviour<NetMessageCenter>,IManage
             {
                 sEvent_NetMessageData tmpNetMessageData = _netMessageDataQueue.Dequeue();
                 handledCnt++;
-                if(handledCnt >=3)
+                try
+                {
+                    if (null != OnMessage)
+                    {
+                        OnMessage(tmpNetMessageData._eventData);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Debug.LogError("try to handle message error!");
+                }
+                if (handledCnt >= perHandleCnt)
                 {
                     break;
                 }
@@ -59,10 +70,11 @@ public class NetMessageCenter : SingletonMonoBehaviour<NetMessageCenter>,IManage
 
     public void Dispose()
     {
-        if(null != _netMessageDataQueue)
+        if (null != _netMessageDataQueue)
         {
             _netMessageDataQueue.Clear();
         }
         _netMessageDataQueue = null;
+        OnMessage = null;
     }
 }
