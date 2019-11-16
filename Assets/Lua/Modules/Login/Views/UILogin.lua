e2d7deb0bca69e63be13e190a09cada59ff6e376
马三小伙儿 <1790617178@qq.com>
2019-11-16 23:03:17 +0800

    添加 ObjectPool Test LObjectPool的使用范例

--- Assets/Lua/Modules/Login/Views/UILogin.lua
+++ Assets/Lua/Modules/Login/Views/UILogin.lua
@@ -31,35 +31,6 @@ function UILogin:OnCreate()
         self:OnNetTest(code,msg)
     end)
     NetManager.RequestSproto(Protocol.C2S_Login,{accountId = 1001,charId = 10086,userName = "Jackson"})
-    -- ObjectPool Test
-    --local LOjectPool = require("Common.Collections.LObjectPool")
-    --
-    --local index = 0
-    --local createAction = function()
-    --    local gameobj = UnityEngine.GameObject.New()
-    --    gameobj.name = "CacheObj" .. index
-    --    index = index + 1
-    --    return gameobj
-    --end
-    --
-    --local releaseAction = function(obj)
-    --    print("------------>ObjType:",type(obj))
-    --    print("---->释放Obj:",obj.name)
-    --end
-    --
-    --local mObjectPool = LOjectPool:New(createAction,releaseAction)
-    --
-    --local obj_1 = mObjectPool:get()
-    --print("-------->第一个物体的名字",obj_1.name)
-    --
-    --local obj_2 = mObjectPool:get()
-    --print("-------->第二个物体的名字",obj_2.name)
-    --
-    --mObjectPool:release(obj_1)
-    --
-    --local obj_3 = mObjectPool:get()
-    --print("-------------->type",type(obj_3))
-    --print("-------->第三个物体的名字",obj_3.name)
 end
 
 -- 界面可见性变化的时候触发
