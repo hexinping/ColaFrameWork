f9ea03b6d277d22d4b4efc0824aa1c4a08886bdd
马三小伙儿 <1790617178@qq.com>
2019-11-22 23:32:22 +0800

    添加DatasetBase

---
+++ Assets/Lua/Core/module/DataSetBase.lua
@@ -0,0 +1,22 @@
+---
+---                 ColaFramework
+--- Copyright © 2018-2049 ColaFramework 马三小伙儿
+---                 XXX模块功能说明
+---
+
+local public = {}
+local private = {}
+
+function public.OnInit()
+
+end
+
+function public.OnExit()
+
+end
+
+function private.Test()
+
+end
+
+return public
