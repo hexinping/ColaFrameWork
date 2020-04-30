b34ed33a5abdb25acaba2e3939c9041f84afb910
XinCGer <1790617178@qq.com>
2020-04-30 16:33:07 +0800

    增加快速打包窗口 ColaBuildWindow

---
+++ Assets/Editor/BuildTools/ColaBuildWindow.cs
@@ -0,0 +1,68 @@
+﻿//----------------------------------------------
+//            ColaFramework
+// Copyright © 2018-2049 ColaFramework 马三小伙儿
+//----------------------------------------------
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using Sirenix.OdinInspector;
+using Sirenix.OdinInspector.Editor;
+
+namespace ColaFramework.ToolKit
+{
+    /// <summary>
+    /// 可视化打包窗口，方便在本地进行打包测试
+    /// </summary>
+    public class ColaBuildWindow : OdinEditorWindow
+    {
+        private static ColaBuildWindow window;
+
+        [LabelText("是否母包")]
+        [SerializeField]
+        [LabelWidth(200)]
+        private bool isMotherPkg;
+
+        [LabelText("是否热更")]
+        [SerializeField]
+        [LabelWidth(200)]
+        private bool isHotUpdate;
+
+        [LabelText("是否是Development Debug包")]
+        [LabelWidth(200)]
+        [SerializeField]
+        private bool isDebug = false;
+
+        [LabelText("是Mono包还是il2cpp包")]
+        [LabelWidth(200)]
+        [SerializeField]
+        private bool isMono = false;
+
+        [LabelText("C#宏定义")]
+        [SerializeField]
+        [LabelWidth(200)]
+        private string CSSymbolDefine;
+
+        [Button("一键打包",ButtonSizes.Large,ButtonStyle.Box)]
+        private void BuildPlayer()
+        {
+
+        }
+
+        private void Init()
+        {
+
+        }
+
+        [MenuItem("Build/快速打包窗口")]
+        public static void PopUp()
+        {
+            window = GetWindow<ColaBuildWindow>("快速打包窗口");
+            window.maximized = false;
+            window.maxSize = window.minSize = new Vector2(500, 300);
+            window.Init();
+            window.Show();
+        }
+    }
+}
