9312884b4881290f07d8348a9d336de66b824e00
XinCGer <1790617178@qq.com>
2020-05-11 20:41:07 +0800

    测试生成AtlasResConfig文件

---
+++ Assets/Lua/Common/AtlasResPathCfg.lua
@@ -0,0 +1,10 @@
+local AtlasResPathCfg = {
+	['bt_buy'] = 'Arts/UI/Atlas/CommonAtlas1/bt_buy.png'
+	['bt_buy_pressed'] = 'Arts/UI/Atlas/CommonAtlas1/bt_buy_pressed.png'
+	['bt_coin'] = 'Arts/UI/Atlas/CommonAtlas1/bt_coin.png'
+	['bt_coin_pressed'] = 'Arts/UI/Atlas/CommonAtlas1/bt_coin_pressed.png'
+	['button_bg'] = 'Arts/UI/Atlas/CommonAtlas1/button_bg.png'
+	['button_bg_pressed'] = 'Arts/UI/Atlas/CommonAtlas1/button_bg_pressed.png'
+	['button_disable_bg'] = 'Arts/UI/Atlas/CommonAtlas1/button_disable_bg.png'
+}
+return AtlasResPathCfg
