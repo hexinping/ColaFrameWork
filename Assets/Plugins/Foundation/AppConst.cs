fd755534064f2761c6b768f1cfe65b1f68b5f394
XinCGer <1790617178@qq.com>
2020-05-15 21:29:22 +0800

    规范化服务器的相关地址

--- Assets/Plugins/Foundation/AppConst.cs
+++ Assets/Plugins/Foundation/AppConst.cs
@@ -28,10 +28,10 @@ public class AppConst
     public const string AppName = "ColaFramework";               //应用程序名称
     public const string ExtName = ".cab";                   //AssetBundle的扩展名
     public const string VersionFileName = "versions.txt";                   //版本信息文件的名称
-    public static string CDNUrl = "http://localhost:6688/";      //CDN地址
-    public static string BakCDNUrl = "http://localhost:6688/";      //备用CDN地址
-    public const string VersionHttpUrl = "http://localhost:6688/{0}/{1}?p={2}&v={3}";  //版本服务器地址
-    public const string BakVersionHttpUrl = "http://localhost:6688/{0}/{1}?p={2}&v={3}"; //备用版本服务器地址
+    public static string CDNUrl = "http://localhost:6688/cdn/";      //CDN地址
+    public static string BakCDNUrl = "http://localhost:6688/cdn/";      //备用CDN地址
+    public const string VersionHttpUrl = "http://localhost:6688/versioncontrol/{0}/{1}?p={2}&v={3}";  //版本服务器地址
+    public const string BakVersionHttpUrl = "http://localhost:6688/versioncontrol/{0}/{1}?p={2}&v={3}"; //备用版本服务器地址
 
     public static string KEY_BASE_APK_VERSION = "__PACKAGE_VERSION__";        // apk的版本号，每次换包以后这个值会被刷新，重新记录
     public static string KEY_APK_BUILD_VERSION = "__PACKAGE_BUILD_VERSION__";  // apk自带的版本号，这个配置存在Resources目录下，除非换包，否则不变
