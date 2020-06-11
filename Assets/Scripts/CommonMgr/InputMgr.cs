bf22eb95a5d0bd6d86b715aaf68f2c92bd137ef6
XinCGer <1790617178@qq.com>
2020-06-11 19:54:22 +0800

    InputMgr加入命名空间

--- Assets/Scripts/CommonMgr/InputMgr.cs
+++ Assets/Scripts/CommonMgr/InputMgr.cs
@@ -7,38 +7,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// ColaFramework框架输入管理器
-/// </summary>
-public class InputMgr : MonoBehaviour
+namespace ColaFramework
 {
-
-    // Use this for initialization
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-        if (Input.GetKey(KeyCode.Escape))
-        {
-            ConfirmQuit();
-        }
-    }
-
     /// <summary>
-    /// 退出游戏前确认对话框
+    /// ColaFramework框架输入管理器
     /// </summary>
-    public void ConfirmQuit()
+    public class InputMgr : MonoBehaviour
     {
-        Application.Quit();
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                ConfirmQuit();
+            }
+        }
+
+        /// <summary>
+        /// 退出游戏前确认对话框
+        /// </summary>
+        public void ConfirmQuit()
+        {
+            Application.Quit();
 #if UNITY_ANDROID && !UNITY_EDITOR
 #endif
+        }
+
     }
-
-
 }
+
 
