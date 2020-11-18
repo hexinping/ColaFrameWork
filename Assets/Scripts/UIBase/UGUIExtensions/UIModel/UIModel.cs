17094a3c2be84253916a18e176b4abd6332f3111
XinCGer <1790617178@qq.com>
2020-11-18 20:48:44 +0800

    增加ModelData模型展示数据类

--- Assets/Scripts/UIBase/UGUIExtensions/UIModel/UIModel.cs
+++ Assets/Scripts/UIBase/UGUIExtensions/UIModel/UIModel.cs
@@ -9,6 +9,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using Sirenix.OdinInspector;
 
 namespace UnityEngine.UI.Extensions
 {
@@ -17,28 +18,59 @@ namespace UnityEngine.UI.Extensions
     /// 不同于UGUIModel组件，该组件使用RenderTexture原理实现
     /// </summary>
     [RequireComponent(typeof(RawImage)), DisallowMultipleComponent]
-    public class UIModel : IControl,IDragHandler
+    public class UIModel : IControl, IDragHandler
     {
+        [LabelText("是否支持拖拽旋转")]
+        public bool isRotate;
+
+        [LabelText("自动旋转速度")]
+        public int autoRotateSpeed = 0;
+        
         void Awake()
         {
-
         }
 
         // Use this for initialization
         void Start()
         {
-
         }
 
         // Update is called once per frame
         void Update()
         {
-
         }
 
         public void OnDrag(PointerEventData eventData)
         {
+        }
+    }
 
+    /// <summary>
+    ///  模型数据
+    /// </summary>
+    [System.Serializable]
+    public class ModelData
+    {
+        public string name;
+        public RectTransform transform;
+
+        private ISceneCharacter _character;
+
+        public ISceneCharacter Character
+        {
+            get { return _character; }
+            set
+            {
+                if (value != _character)
+                {
+                    if (_character.isNotNull())
+                    {
+                        _character.Release();
+                    }
+
+                    _character = value;
+                }
+            }
         }
     }
 }
